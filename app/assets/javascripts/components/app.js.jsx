f57a3543728b3f15041e48d16d82b764ad9b390e
Philip Cornman <pjc5108@gmail.com>
2017-07-23 07:26:18 -0700

    set up app react component. renders correctly in view.

---
+++ app/assets/javascripts/components/app.js.jsx
@@ -0,0 +1,14 @@
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return(
+    <div className="app">
+      <h1>Healthy mind, healthy time.</h1>
+    </div>
+    )
+  }
+}
