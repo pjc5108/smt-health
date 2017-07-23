737b10755f1f929f4715467aaa88a0460aa00ac0
Philip Cornman <pjc5108@gmail.com>
2017-07-23 07:30:04 -0700

    set up navbar component, still needs to be written.

---
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -0,0 +1,14 @@
+class NavBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render(){
+    return(
+      <div className="navbar">
+        
+      </div>
+    )
+  }
+}
