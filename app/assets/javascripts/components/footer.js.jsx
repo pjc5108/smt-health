d14a76f1fdf666ba7e6e65f2f47faa7e4adcb2ab
Philip Cornman <pjc5108@gmail.com>
2017-08-15 08:34:49 -0700

    create skeleton of footer.

---
+++ app/assets/javascripts/components/footer.js.jsx
@@ -0,0 +1,17 @@
+class Footer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  render() {
+    return(
+    <div className="footer">
+      <p>I'm working.</p>
+
+
+    </div>
+    )
+  }
+}
