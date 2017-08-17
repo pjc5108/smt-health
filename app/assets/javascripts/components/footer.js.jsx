ab78d742c72bf3eba520643c48f66896ea6654f4
Philip Cornman <pjc5108@gmail.com>
2017-08-17 07:07:10 -0700

    add some sample column headers in footer

--- app/assets/javascripts/components/footer.js.jsx
+++ app/assets/javascripts/components/footer.js.jsx
@@ -8,7 +8,22 @@ class Footer extends React.Component {
   render() {
     return(
     <div className="footer">
-      <p>I'm working.</p>
+      <div className="col-xs-3 footer-column">
+        <h5 className="footer-column-header">About</h5>
+
+      </div>
+      <div className="col-xs-3 footer-column">
+        <h5 className="footer-column-header">Links</h5>
+
+      </div>
+      <div className="col-xs-3 footer-column">
+        <h5 className="footer-column-header">Resources</h5>
+
+      </div>
+      <div className="col-xs-3 footer-column">
+        <h5 className="footer-column-header">Contact</h5>
+
+      </div>
 
 
     </div>
