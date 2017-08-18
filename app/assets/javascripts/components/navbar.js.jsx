425f9a9682a35c5b8c9b35f53ee2f81d95ac94c6
Philip Cornman <pjc5108@gmail.com>
2017-08-18 13:45:54 -0700

    testing some alternative solutions for building a working navbar.

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -6,18 +6,49 @@ class NavBar extends React.Component {
 
   render(){
     return(
-      <div className="full-navbar">
-        <nav className="navbar navbar-default navbar-upper">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-          </div>
+      <nav className="health-navbar">
+        <ul className="nav navbar-nav nav-list">
+          <li><a href="#">Home</a></li>
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Health Counseling <span className="caret"></span></a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Program 1</a></li>
+                <li><a href="#">Program 2</a></li>
+                <li><a href="#">Program 3</a></li>
 
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
+              </ul>
+            </li>
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Yoga <span className="caret"></span></a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Private Session</a></li>
+                <li><a href="#">Group Session</a></li>
+                <li><a href="#">Corporate Yoga</a></li>
+              </ul>
+            </li>
+          <li><a href="#">JuicePlus+</a></li>
+          <li><a href="#">Products</a></li>
+          <li><a href="#">Blog</a></li>
+          <li className="left-divider"><a href="#">Testimonials</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+
+
+
+
+
+
+        {/* This is inside the div so that it can actually run. May revert back to the bootstrap nav if I can get it working. */}
+        {/*<nav className="navbar navbar-default">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+
+          <div className="collapse navbar-collapse aaaa" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav bbbb">
               <li><a href="#">Home</a></li>
                 <li className="dropdown">
                   <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Health Counseling <span className="caret"></span></a>
@@ -25,7 +56,7 @@ class NavBar extends React.Component {
                     <li><a href="#">Program 1</a></li>
                     <li><a href="#">Program 2</a></li>
                     <li><a href="#">Program 3</a></li>
-                    {/*}<li role="separator" className="divider"></li>*/}
+
                   </ul>
                 </li>
                 <li className="dropdown">
@@ -44,11 +75,9 @@ class NavBar extends React.Component {
               <li><a href="#">Contact</a></li>
             </ul>
           </div>
-        </nav>
-        <div className="navbar-lower">
+        </nav>*/}
+      </nav>
 
-        </div>
-      </div>
     )
   }
 }
