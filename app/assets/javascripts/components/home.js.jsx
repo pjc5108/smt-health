1af1214c4dbd0b09e3255faf660e5842eea69944
Philip Cornman <pjc5108@gmail.com>
2017-10-06 23:48:37 -0700

    Create navbar links, edit navbar fonts, and create dropdown menu for health counseling that mostly works correctly.

---
+++ app/assets/javascripts/components/home.js.jsx
@@ -0,0 +1,20 @@
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+// this component should contain mostly static information about the business/simone
+
+  render() {
+    return(
+      <div className="home-container">
+        <div className="introduction">
+          <p>"Welcome to my site, I'm trying to make you healthy so listen to me. Take lots of juiceplus++++ LOL"</p>
+        </div>
+
+
+      </div>
+
+    )
+  }
+}
--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -7,75 +7,23 @@ class NavBar extends React.Component {
   render(){
     return(
       <nav className="health-navbar">
-        <ul className="nav navbar-nav nav-list">
-          <li><a href="#">Home</a></li>
-            <li className="dropdown">
-              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Health Counseling <span className="caret"></span></a>
-              <ul className="dropdown-menu">
-                <li><a href="#">Program 1</a></li>
-                <li><a href="#">Program 2</a></li>
-                <li><a href="#">Program 3</a></li>
-
-              </ul>
-            </li>
-            <li className="dropdown">
-              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Yoga <span className="caret"></span></a>
-              <ul className="dropdown-menu">
-                <li><a href="#">Private Session</a></li>
-                <li><a href="#">Group Session</a></li>
-                <li><a href="#">Corporate Yoga</a></li>
-              </ul>
-            </li>
-          <li><a href="#">JuicePlus+</a></li>
-          <li><a href="#">Products</a></li>
-          <li><a href="#">Blog</a></li>
-          <li className="left-divider"><a href="#">Testimonials</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-
-
-
-
-
-
-        {/* This is inside the div so that it can actually run. May revert back to the bootstrap nav if I can get it working. */}
-        {/*<nav className="navbar navbar-default">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-
-          <div className="collapse navbar-collapse aaaa" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav bbbb">
-              <li><a href="#">Home</a></li>
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Health Counseling <span className="caret"></span></a>
-                  <ul className="dropdown-menu">
-                    <li><a href="#">Program 1</a></li>
-                    <li><a href="#">Program 2</a></li>
-                    <li><a href="#">Program 3</a></li>
-
-                  </ul>
-                </li>
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Yoga <span className="caret"></span></a>
-                  <ul className="dropdown-menu">
-                    <li><a href="#">Private Session</a></li>
-                    <li><a href="#">Group Session</a></li>
-                    <li><a href="#">Corporate Yoga</a></li>
-                  </ul>
-                </li>
-              <li><a href="#">Juice+</a></li>
-              <li><a href="#">Products</a></li>
-              <li><a href="#">Blog</a></li>
-              <li className="left-divider"><a href="#">Testimonials</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
+        <div className="link-container">
+          <a className="navbar-link" href="#">Home</a>
+          <div className="dropdown">
+            <a className="navbar-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Health Counseling <span className="caret"></span></a>
+            <ul className="dropdown-menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another Action</a></li>
             </ul>
           </div>
-        </nav>*/}
+          <a className="navbar-link" href="#">Yoga <span className="caret"></span></a>
+          <a className="navbar-link" href="#">JuicePlus+</a>
+          <a className="navbar-link" href="#">Products</a>
+          <a className="navbar-link" href="#">Blog</a>
+          <a className="left-divider navbar-link" href="#">Testimonials</a>
+          <a className="navbar-link" href="#">About</a>
+          <a className="navbar-link" href="#">Contact</a>
+        </div>
       </nav>
 
     )
