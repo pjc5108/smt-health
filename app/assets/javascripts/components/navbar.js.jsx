231e75a97b38e54ec19b48c72f90b3ae5ff8d519
Philip Cornman <pjc5108@gmail.com>
2017-10-07 17:32:35 -0700

    Add border to link container, add banner and text above link container.

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -6,25 +6,33 @@ class NavBar extends React.Component {
 
   render(){
     return(
-      <nav className="health-navbar">
-        <div className="link-container">
-          <a className="navbar-link" href="#">Home</a>
-          <div className="dropdown">
-            <a className="navbar-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Health Counseling <span className="caret"></span></a>
-            <ul className="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another Action</a></li>
-            </ul>
-          </div>
-          <a className="navbar-link" href="#">Yoga <span className="caret"></span></a>
-          <a className="navbar-link" href="#">JuicePlus+</a>
-          <a className="navbar-link" href="#">Products</a>
-          <a className="navbar-link" href="#">Blog</a>
-          <a className="left-divider navbar-link" href="#">Testimonials</a>
-          <a className="navbar-link" href="#">About</a>
-          <a className="navbar-link" href="#">Contact</a>
+      <div>
+        <div className="top-banner">
+          <p className="top-banner-text">
+            Simone's Complete Health
+          </p>
         </div>
-      </nav>
+        <nav className="health-navbar">
+          <div className="link-container">
+            <a className="navbar-link" href="#">Home</a>
+            <div className="dropdown">
+              <a className="navbar-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Health Counseling <span className="caret"></span></a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Private Counseling</a></li>
+                <li><a href="#">Corporate Counseling</a></li>
+              </ul>
+            </div>
+            <a className="navbar-link" href="#">Yoga <span className="caret"></span></a>
+            <a className="navbar-link" href="#">JuicePlus+</a>
+            <a className="navbar-link" href="#">Products</a>
+            <a className="navbar-link" href="#">Blog</a>
+            <a className="navbar-link" href="#">Testimonials</a>
+            <a className="navbar-link" href="#">About</a>
+            <a className="navbar-link" href="#">Contact</a>
+          </div>
+        </nav>
+      </div>
+
 
     )
   }
